10188ea0f5da2f38442b96c9d93528bdcf4ca65e
MartaMykhailyna <marta.mykhailyna-pi2002@kep.nung.edu.ua>
2024-06-01 19:09:05 +0300

    ready for deploy

--- manager/manager/settings.py
+++ manager/manager/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'manager.urls'
@@ -125,11 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'manager_app/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'manager_app/static/'),
-
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage' 
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
